decc296d9581ac95ce2b5c69287819fab13c1f88
sgnanasekar <sakunthalagnanasekar@gmail.com>
2021-06-23 16:08:11 +0100

    SearchAutoComplete

--- src/SearchAutoComplete.java
+++ src/SearchAutoComplete.java
@@ -53,7 +53,7 @@ class Result {
     }
 
 }
-public class Solution {
+public class SearchAutoComplete {
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
@@ -89,6 +89,6 @@ public class Solution {
                 });
 
         bufferedReader.close();
-       // bufferedWriter.close();
+        bufferedWriter.close();
     }
 }
